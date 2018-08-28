93374b92f8c0d1de8a0f38b05ae6e111497e7534
Ximi Hoque <hoque.ximi@gmail.com>
2018-08-28 22:37:26 +0530

    rating behaviour added

--- code/hotspots.py
+++ code/hotspots.py
@@ -15,7 +15,7 @@ def read_data(fname):
 def calculate_deviations(df, speed_dict):
     logging.debug("Calculating")
     deviations = df.apply(lambda x: speed_dict['deviceCode_pyld_speed'][x['deviceCode_pyld_alarmType']] - x['deviceCode_pyld_speed'], axis=1)
-    df['deviations'] = deviations
+    df['hotspot_magnitude'] = deviations
     return df
 
 def main():
@@ -44,11 +44,18 @@ def main():
         deviated_df = deviated_df.append(calculate_deviations(df_, speed_dict))
     
     #Scaling data 0-100
-    deviated_df['deviations'] *= 100.0/deviated_df['deviations'].min()
-    deviated_df['deviations'] = deviated_df['deviations'].apply(lambda x: x if x>0 else 0)
+    deviated_df['hotspot_magnitude'] *= 100.0/deviated_df['hotspot_magnitude'].min()
+    deviated_df['hotspot_magnitude'] = deviated_df['hotspot_magnitude'].apply(lambda x: x if x>0 else 0)
+    
+    #Rating behaviour of Drivers 0-10
+    rating = deviated_df[['deviceCode_deviceCode',  'hotspot_magnitude']].groupby('deviceCode_deviceCode').agg('mean').reset_index()
+    rating['hotspot_magnitude'] *= 10.0/rating['hotspot_magnitude'].max()
+    rating['hotspot_magnitude'] = 10 - rating['hotspot_magnitude']
     
     #Exporting
     logging.info("Exporting...")
     deviated_df.to_csv('hotspots.csv', index=False)
+    rating.to_csv('rating.csv', index=False)
+    
 if __name__ == '__main__':
     main()
